e2df4256eb837486fe87a28c5db7a08c93a7fd08
tangxiaoyu <tangxiaoyu@mafengwo.com>
2021-09-29 18:56:52 +0800

    创建工程

---
+++ SourceTreeTest/Shared/SourceTreeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceTreeTestApp.swift
+//  Shared
+//
+//  Created by 唐晓宇 on 2021/9/29.
+//
+
+import SwiftUI
+
+@main
+struct SourceTreeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

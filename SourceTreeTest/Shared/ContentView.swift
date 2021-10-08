bfa91c021fbcc9587b69711886b6fa7e0a142154
tangxiaoyu <tangxiaoyu0219@sina.com>
2021-10-08 14:19:35 +0800

    文件夹分支测试

---
+++ SourceTreeTest/Shared/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 唐晓宇 on 2021/9/29.
+//
+
+import SwiftUI
+
+func0()
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+    
+}
+
+func()
+
+func1()
